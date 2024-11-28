e759f396d14d89a5bdfec0b977ada4e504f02980
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2024-11-27 17:55:28 -0700

    Added Discount and Promos Page

---
+++ src/types/DiscountItem.ts
@@ -0,0 +1,11 @@
+
+type DiscountItem =
+{
+    name: string,
+    url?: string,
+    description?: string
+    image?: string,
+    preferredFontSize?: string | number,
+}
+
+export default DiscountItem
