bf9434a8e5cc1ddc86bd156baa47d1cd0240c9cc
Carter <cartermar211@gmail.com>
2024-12-07 23:48:30 -0700

    minor last minute changes

--- src/components/NavBar/CallServerButton.tsx
+++ src/components/NavBar/CallServerButton.tsx
@@ -21,7 +21,7 @@ const CallServerButton = React.memo(({onClick}:{onClick: ()=>void})=>
 
     return (
 
-            <Fab sx={{position: 'fixed', borderRadius: 10, height: 'auto', bottom: '1%', left: '0%', fontWeight: 1000, fontSize: 14, zIndex: 1001, 
+            <Fab sx={{position: 'fixed', borderRadius: 10, height: 'auto', bottom: '1.5%', left: 3, fontWeight: 1000, fontSize: 14, zIndex: 1001, 
                     backgroundColor: theme.palette.dennysYellow.main, 
                     color: theme.palette.dennysYellow.contrastText,
                     '&.MuiButtonBase-root':
